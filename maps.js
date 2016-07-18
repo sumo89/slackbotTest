f44dc02b194912d2a750653cb9e53def324eb99f
Henry Summers <ssumo89@gmail.com>
2016-07-18 22:56:33 +0100

    fixed the for loop

--- maps.js
+++ maps.js
@@ -4,28 +4,19 @@ module.exports = function(robot){
 	robot.hear(/show me (.*)/i, function(msg){
 		var textEntry = msg.match[1];
 		request('https://maps.googleapis.com/maps/api/place/textsearch/json?&location=51.518565,-0.125537&radius=10000&query=' + textEntry + '&key=AIzaSyAFCkSucNqbLFOZoJBEkmbmb2_7TBING5g', function (error, response, body){
+			
 			var mapResult = JSON.parse(body);
-			var resultNameLocation = mapResult.results[0].name + mapResult.results[0].formatted_address;
-			//need to do a for loop, array .join()
+			var MapResultName = [];
+			for (var i = 0; i < mapResult.results.length; i++){
+				 MapResultName.push ("\n" + mapResult.results[i].name + " - " + mapResult.results[i].formatted_address);
+			}
+			return msg.send("Here are some " + textEntry + ": " + MapResultName);
 
-			for (var i = 0; i < resultNameLocation.length; i ++){
-				return msg.send("Here are some " + textEntry + ": " + resultNameLocation[i]);
-			};
-
-			// return msg.send(mapResult);
-				// for (var key in mapResult){
-				// 	if (mapResult.hasOwnProperty(key)){
-				// 		console.log(mapResult[key]);
-				// 	}
-				// }
-
-				// return msg.send("Here are some " + textEntry + ": " + mapResult.results[0].name);
+		
 
 
 		});
 	});
 };
-
-// JSON.stringify(textEntry);
 
 
